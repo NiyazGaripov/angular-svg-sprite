968bec7ec0514238a3e8e299e8134a9b147a1c55
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-01-10 23:04:08 +0300

    feat: adds svg-icon component in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SvgIconComponent } from './components/svg-icon/svg-icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SvgIconComponent
   ],
   imports: [
     BrowserModule
