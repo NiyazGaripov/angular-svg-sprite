f942abccb6cd46c1a80b04ca21262b80b640e137
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-01-10 23:24:05 +0300

    feat: adds input property

--- src/app/components/svg-icon/svg-icon.component.ts
+++ src/app/components/svg-icon/svg-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-svg-icon',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./svg-icon.component.scss']
 })
 export class SvgIconComponent implements OnInit {
+
+  @Input() icon!: string;
 
   constructor() { }
 
